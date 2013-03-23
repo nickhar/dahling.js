116f0dc1e2aef0d20b73ebe70f7ebaf149f517c6
nickhar <nickharrison@gmail.com>
2013-03-23 03:16:30 +0000

    initial file

---
+++ dahling.js
@@ -0,0 +1,68 @@
+
+/*
+ *
+ *
+ * dahling.js
+ *
+ * It's just media dahling...
+ *
+ *
+ */
+
+var dahling = (function(){
+ 
+    var div;
+	
+	return function(query){
+		if (!div){
+			div = document.createElement("div");
+			div.id = "dahling";
+			div.style.cssText = "position:absolute;top:-1000px";
+			document.body.insertBefore(div, document.body.firstChild);			
+		}
+	
+		div.innerHTML = "_<style media=\"" + query + "\"> #dahling { width: 1px; }</style>";
+		div.removeChild(div.firstChild);
+		return div.offsetWidth == 1;	
+	};
+})();
+ 
+/*
+ * Simple dahling usage, just pass in a CSS media query.
+ *
+ *	if (isMedia("screen and (max-width:800px)"){
+ *	    //do something for the screen dahling
+ *	}
+ *	 
+ *	if (isMedia("all and (orientation:portrait)")){
+ *	    //react to portrait mode dahling
+ *	}
+ *
+ */
+
+
+function loadJS(jsname,pos) {
+var th = document.getElementsByTagName(pos)[0];
+var s = document.createElement('script');
+s.setAttribute('type','text/javascript');
+s.setAttribute('src',jsname);
+th.appendChild(s);
+}
+
+/*
+ * Ulterior motive and usage dahling:
+ *
+ *	if (dahling("screen and (max-width:800px)"){
+ *	    //do something for gods sake
+ *	        console.log("Less than 800px");
+ *		loadJS('/js/bootstrap-spopover.js','body');
+ *	}
+ *
+ *
+ */
+
+
+
+
+
+
