f22ef481b2720ebd2f21661ee5c882d80429279e
nickhar <nickharrison@gmail.com>
2013-03-24 01:06:23 +0000

    update

--- dahling.js
+++ dahling.js
@@ -1,8 +1,12 @@
 /*
- *
  * dahling.js
+ * Dahling, It's just media queries...
  *
- * It's just media dahling...
+ * By Nick Harrison | @nickhar
+ * https://github.com/nickhar/dahling.js
+ *
+ * License : GPLv2 & MIT
+ * Forks, clones and contributions welcome
  *
  */
 
@@ -13,22 +17,96 @@ var dahling = (function(){
     return function(query){
 	    if (!div){
 		    div = document.createElement("div");
-		    div.id = "dahling";
+		    div.id = "dahlingjs";
 		    div.style.cssText = "position:absolute;top:-1000px";
 		    document.body.insertBefore(div, document.body.firstChild);			
 	    }
     
-	    div.innerHTML = "_<style media=\"" + query + "\"> #dahling { width: 1px; }</style>";
+	    div.innerHTML = "_<style media=\"" + query + "\"> #dahlingjs { width: 1px; }</style>";
 	    div.removeChild(div.firstChild);
 	    return div.offsetWidth == 1;	
+    
     };
 
 })();
- 
-    function loadJS(jsname,pos) {
-	var th = document.getElementsByTagName(pos)[0];
-	var s = document.createElement('script');
-	s.setAttribute('type','text/javascript');
-	s.setAttribute('src',jsname);
-	th.appendChild(s);
-    }
+
+
+
+// file loader
+    function load(filename)
+    {
+
+
+	if(filename.constructor == Array)
+	{
+		//console.log(filename);
+    
+    
+	    for (index = 0; index < filename.length; ++index)
+	    {
+		//console.log(filename[index]);
+    
+    
+	// Get file extension
+	    var ext = filename[index].split('?')[0];
+	    ext = ext.substr(ext.lastIndexOf('.')+1);
+	    
+	    if(ext == "js" || ext == "JS" || ext == "css" || ext == "CSS")
+	    {
+		filetype = ext;
+    
+	    
+		if (filetype == "js") { //if filename is external JS file
+		    var fileref = document.createElement('script')
+		    fileref.setAttribute("type", "text/javascript")
+		    fileref.setAttribute("src", filename[index])
+		    //alert('called');
+		}
+		else if (filetype == "css") { //if filename is external CSS file
+		    var fileref = document.createElement("link")
+		    fileref.setAttribute("rel", "stylesheet")
+		    fileref.setAttribute("type", "text/css")
+		    fileref.setAttribute("href", filename[index])
+		}
+		if (typeof fileref != "undefined")
+		    document.getElementsByTagName("head")[0].appendChild(fileref)
+	    }
+    
+    
+	}
+    
+    
+    
+    } else {
+    
+    
+	    
+	// Get single ile extension
+	    var ext = filename.split('?')[0];
+	    ext = ext.substr(ext.lastIndexOf('.')+1);
+	
+	    if(ext == "js" || ext == "JS" || ext == "css" || ext == "CSS")
+	    {
+		filetype = ext;
+    
+	    
+		if (filetype == "js") { //if filename is external JS file
+		    var fileref = document.createElement('script')
+		    fileref.setAttribute("type", "text/javascript")
+		    fileref.setAttribute("src", filename)
+		    //alert('called');
+		}
+		else if (filetype == "css") { //if filename is external CSS file
+		    var fileref = document.createElement("link")
+		    fileref.setAttribute("rel", "stylesheet")
+		    fileref.setAttribute("type", "text/css")
+		    fileref.setAttribute("href", filename)
+		}
+		if (typeof fileref != "undefined")
+		    document.getElementsByTagName("head")[0].appendChild(fileref)
+	    }
+    
+    }	
+}
+	
+	
